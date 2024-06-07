699cffe45d884db79bacf24b890ce1ed819995e4
hadi <rasoulihadi101@gmail.com>
2024-06-07 19:53:48 +0330

    12...video 13

--- component/Places/ImagePicker.js
+++ component/Places/ImagePicker.js
@@ -1,24 +1,86 @@
-import { launchCameraAsync } from "expo-image-picker";
-import { Button, View } from "react-native";
+import {
+  MediaTypeOptions,
+  PermissionStatus,
+  launchCameraAsync,
+  useCameraPermissions,
+} from "expo-image-picker";
+import { useState } from "react";
+import { Alert, Button, Image, StyleSheet, Text, View } from "react-native";
+import { Colors } from "../../constants/color";
+import OutlinedButton from "../UI/OutlinedButton";
 
 const ImagePicker = () => {
+  const [pickedImage, setPickedImage] = useState();
+
+  const [cameraPermissionInformation, requestPermission] =
+    useCameraPermissions();
+  // این یوز کمرا پرمیشن به ما دو تا المان میدهد که یکی اطلاعات مجوز عکس رو به ما میدهد  و یکی درخواستی که برای مجوز صادر میشود را هندل میکنه
+
+  const verifyPermision = async () => {
+    if (cameraPermissionInformation.status === PermissionStatus.UNDETERMINED) {
+      // اگر مجز در حالت نامعلومی قرار داشت پایین عمل میکند
+      const permissionResponse = await requestPermission();
+      return permissionResponse.granted;
+    }
+
+    if (cameraPermissionInformation.status === PermissionStatus.DENIED) {
+      // اگر مجوز رد شده بود این پایین را انجام میدهد
+      Alert.alert(
+        "Insufficient Permissioms!",
+        "You need to grant camera permissions to use this app"
+      );
+      return false;
+    }
+    // اگر هیچکدام از حالت های بالا نبود ترو بر میگرداند که البته امکان ندارد
+    return true;
+  };
+  // این قسمت بالا در واقع برای آی او اس است و در سامسونگ اینها به صورت اتومات تنظیم شده اند
   const takeImageHandler = async () => {
+    const hasPermission = await verifyPermision();
+    if (!hasPermission) {
+      return;
+    }
+    // در واقع در اینجا گفتیم اگه در ای اوس اجازه صادر نشده از این فانکشن خارج شو
+
     const image = await launchCameraAsync({
       allowsEditing: true,
       aspect: [16, 9],
       quality: 0.5,
-      // در داخل این که به صورت ابجکتی شکستیم میتوانیم برخی تنظیمات را اضافه کنیم مثل ادیت عکس سایز عکس و کیفیت عکس و ...
     });
-    // بعد از نصب امیچ پیکر و قرار دادن برخی اجازه ها در اپ دات جیسون اینجا با استفاده لانچ کمرا ایسینک از ان استفاده کردیم و فانکشنی که هم استفاده کردیم پرامیسی کردیم بعد از گرفتن عکس برخی کار هار بکند چون ممکن است طرف عکسی نندازد در ضمن مجوز هایی که گفتن فقط در اندروید کار کرد زیرا خودش به صورت اتومات انجام میدهد برای ای او اس بعدا به صورت دستی این مجوز هارا ست میکینم تا دوربین کار کند
-    // در ضمن ما میتوانستیم از اکسپو کمرا هم استفاده کنیم که به ما امکان شخصی سازی کمرا را میدهد ولی برای گرفتن عکس ساده یا برداشتن عکس از گالری همین امیج پیکر کافی هست
-    console.log(image);
+    // console.log(image);
+    setPickedImage(image.assets[0].uri);
+    // ادرس عکسی که انداختیم را در یک استیت ذخیره میکنیم و در قسمت سورس عکس از ان استیت برای نمایش عکس استفاده میکنیم
   };
+  let imagePreview = <Text>No image taken yet.</Text>;
+
+  if (pickedImage) {
+    imagePreview = <Image style={styles.image} source={{ uri: pickedImage }} />;
+  }
   return (
     <View>
-      <View></View>
-      <Button title="Take Image" onPress={takeImageHandler} />
+      <View style={styles.imagePreview}>{imagePreview}</View>
+      {/* <Button title="Take Image" onPress={takeImageHandler} /> */}
+      <OutlinedButton icon="camera" onPressFunc={takeImageHandler}>
+        Take Image
+      </OutlinedButton>
     </View>
   );
 };
 
 export default ImagePicker;
+
+const styles = StyleSheet.create({
+  imagePreview: {
+    width: "100%",
+    height: 200,
+    marginVertical: 8,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: Colors.primary100,
+    borderRadius: 4,
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+  },
+});
