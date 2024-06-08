f660401e285413b72bad9bc85f975cb248a450f7
hadi <rasoulihadi101@gmail.com>
2024-06-08 16:36:39 +0330

    12...video 15

--- component/Places/ImagePicker.js
+++ component/Places/ImagePicker.js
@@ -14,33 +14,27 @@ const ImagePicker = () => {
 
   const [cameraPermissionInformation, requestPermission] =
     useCameraPermissions();
-  // این یوز کمرا پرمیشن به ما دو تا المان میدهد که یکی اطلاعات مجوز عکس رو به ما میدهد  و یکی درخواستی که برای مجوز صادر میشود را هندل میکنه
 
   const verifyPermision = async () => {
     if (cameraPermissionInformation.status === PermissionStatus.UNDETERMINED) {
-      // اگر مجز در حالت نامعلومی قرار داشت پایین عمل میکند
       const permissionResponse = await requestPermission();
       return permissionResponse.granted;
     }
 
     if (cameraPermissionInformation.status === PermissionStatus.DENIED) {
-      // اگر مجوز رد شده بود این پایین را انجام میدهد
       Alert.alert(
         "Insufficient Permissioms!",
         "You need to grant camera permissions to use this app"
       );
       return false;
     }
-    // اگر هیچکدام از حالت های بالا نبود ترو بر میگرداند که البته امکان ندارد
     return true;
   };
-  // این قسمت بالا در واقع برای آی او اس است و در سامسونگ اینها به صورت اتومات تنظیم شده اند
   const takeImageHandler = async () => {
     const hasPermission = await verifyPermision();
     if (!hasPermission) {
       return;
     }
-    // در واقع در اینجا گفتیم اگه در ای اوس اجازه صادر نشده از این فانکشن خارج شو
 
     const image = await launchCameraAsync({
       allowsEditing: true,
@@ -49,7 +43,6 @@ const ImagePicker = () => {
     });
     // console.log(image);
     setPickedImage(image.assets[0].uri);
-    // ادرس عکسی که انداختیم را در یک استیت ذخیره میکنیم و در قسمت سورس عکس از ان استیت برای نمایش عکس استفاده میکنیم
   };
   let imagePreview = <Text>No image taken yet.</Text>;
 
@@ -59,7 +52,6 @@ const ImagePicker = () => {
   return (
     <View>
       <View style={styles.imagePreview}>{imagePreview}</View>
-      {/* <Button title="Take Image" onPress={takeImageHandler} /> */}
       <OutlinedButton icon="camera" onPressFunc={takeImageHandler}>
         Take Image
       </OutlinedButton>
@@ -78,9 +70,11 @@ const styles = StyleSheet.create({
     alignItems: "center",
     backgroundColor: Colors.primary100,
     borderRadius: 4,
+    overflow: "hidden",
   },
   image: {
     width: "100%",
     height: "100%",
+    borderRadius: 4,
   },
 });
--- component/Places/LocationPicker.js
+++ component/Places/LocationPicker.js
@@ -1,14 +1,66 @@
-import { StyleSheet, View } from "react-native";
+import { Alert, Image, StyleSheet, Text, View } from "react-native";
 import OutlinedButton from "../UI/OutlinedButton";
 import { Colors } from "../../constants/color";
+import {
+  getCurrentPositionAsync,
+  useForegroundPermissions,
+} from "expo-location";
+import { useState } from "react";
 
 const LocationPicker = () => {
-  const getLocationHandler = () => {};
+  const [pickedLocation, setPickedLocation] = useState();
+
+  const verifyPermissions = async () => {
+    const [locationPermissionInformation, requestPermission] =
+      useForegroundPermissions();
+
+    if (
+      locationPermissionInformation.status === PermissionStatus.UNDETERMINED
+    ) {
+      const permissionResponse = await requestPermission();
+      return permissionResponse.granted;
+    }
+
+    if (locationPermissionInformation.status === PermissionStatus.DENIED) {
+      Alert.alert(
+        "Insufficient Permissioms!",
+        "You need to grant location permissions to use this app"
+      );
+      return false;
+    }
+    return true;
+  };
+  // در لوکیشن برای هر دو پلتفورم اجازه گرفتن دستی نیاز است و همان کار های اجازه برای عکس ایفونی که قبلا انجام دادیم را اینبار برای لوکیشن هر دو پلتفورم استفاده میکنیم
+
+  const getLocationHandler = async () => {
+    const hasPermission = await verifyPermissions();
+    if (!hasPermission) {
+      return;
+    }
+    const location = await getCurrentPositionAsync();
+    setPickedLocation({
+      lat: location.coords.latitude,
+      lng: location.coords.longitude,
+    });
+  };
   const pickOnMapHandler = () => {};
+
+  let locationPreview = <Text>No location picked yet</Text>;
+
+  if (pickedLocation) {
+    locationPreview = (
+      <Image
+        style={styles.image}
+        source={{
+          uri: "همون ای پی ایی که از گوگل مپ دریافت کردیم و لت و لانگ خودمان ازینجا استخراج کردیم را بهش  دادیم و در اون ادرس اچ تی تی پی قرار دادیم ",
+        }}
+      />
+    );
+  }
 
   return (
     <View>
-      <View style={styles.mapPreview}></View>
+      <View style={styles.mapPreview}>{locationPreview}</View>
       <View style={styles.actions}>
         <OutlinedButton onPressFunc={getLocationHandler} icon="location">
           Locate User
@@ -32,10 +84,16 @@ const styles = StyleSheet.create({
     alignItems: "center",
     backgroundColor: Colors.primary100,
     borderRadius: 4,
+    overflow: "hidden",
   },
   actions: {
     flexDirection: "row",
     justifyContent: "space-around",
     alignItems: "center",
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+    borderRadius: 4,
   },
 });
